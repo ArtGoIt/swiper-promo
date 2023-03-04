f9f1f25f5c7329e575d9a701bb2974f97119da49
Seva1986 <a.severin@goit.ua>
2023-03-04 17:42:14 +0200

    slider added

---
+++ src/js/slider.js
@@ -0,0 +1,24 @@
+import Swiper from 'swiper/bundle';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.slider-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
