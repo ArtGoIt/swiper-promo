d79b25bd6c250b70cc42ede0106cffef2bd383b2
ArtGoit <a.severin@goit.ua>
2023-04-09 11:24:38 +0300

    add slid

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,1 +1,12 @@
+import Swiper from 'swiper/bundle';
 
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 100,
+
+  pagination: {
+    el: '.slider-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
